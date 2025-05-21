780622e50f395ed2fa17914b062f50496bd04da3
Kadar A <ka.sharif01@gmail.com>
2025-05-21 01:55:38 +0000

    Update app.py

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world.. Al hamdulilah I have just created my first docker based project \
-    where I'm using docker container to run my web application of hello-flask which is scripted in Python!'
+    return 'Hello, world.. Al hamdulilah I have just created my first docker based project where I'm using docker container to run my web application of hello-flask which is scripted in Python!'
 
 if  __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
